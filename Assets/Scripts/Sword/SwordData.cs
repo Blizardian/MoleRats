badf08cbbfced7d6edfbd2bceedd30ea94e2a134
Tafelpoot <aidendehoogh1@hotmail.com>
2023-05-23 11:59:55 +0200

    Weapon sway added
    verry nice

---
+++ Assets/Scripts/Sword/SwordData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Gun", menuName = "Weapon/Gun")]
+public class GunData : ScriptableObject
+{
+
+    [Header("Info")]
+    public new string name;
+
+    [Header("Shooting")]
+    public float damage;
+    public float maxDistance;
+
+    
+
+}
